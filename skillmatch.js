43e306823a0f273b66927888caadb27dbe30e8fe
Caua_Costa18 <caualcosta0408@gmail.com>
2026-05-20 19:34:07 -0300

    feat: implementa cálculo de compatibilidade

--- skillmatch.js
+++ skillmatch.js
@@ -34,3 +34,42 @@ const vagas = [
     nivel: "Júnior"
     }
 ];
+
+function calcularCompatibilidade(candidato, vaga) {
+  const requisitosAtendidos = [];
+  const requisitosNaoAtendidos = [];
+
+  for (const requisito of vaga.requisitos) {
+    if (candidato.habilidades.includes(requisito)) {
+      requisitosAtendidos.push(requisito);
+    } else {
+      requisitosNaoAtendidos.push(requisito);
+    }
+  }
+
+  const compatibilidade =
+    (requisitosAtendidos.length /
+      vaga.requisitos.length) * 100;
+
+  let classificacao = "";
+
+  if (compatibilidade >= 80) {
+    classificacao = "Alta compatibilidade";
+  } else if (compatibilidade >= 50) {
+    classificacao = "Média compatibilidade";
+  } else {
+    classificacao = "Baixa compatibilidade";
+  }
+
+  console.log("Empresa: "+vaga.empresa+
+    "\nCargo: "+vaga.cargo+
+    "\mCompatibilidade: "+compatibilidade+"%"+
+    "\nHabilidades encontradas: "+requisitosAtendidos.join(", ")+
+    "\nHabilidades faltantes: "+requisitosNaoAtendidos.join(", ")+
+    "\nClassificação: "+classificacao
+  );
+}
+
+for (const vaga of vagas) {
+  calcularCompatibilidade(candidato, vaga);
+}
